fe9b20a95394caa84990ffe4d44ad3f3ff36f32e
keita <developer@milkcocoa.info>
2023-11-25 22:42:13 +0900

    [WIP]とりあえずCropの窓とか

--- milkrop/src/main/java/com/milkcocoa/info/milkrop/view/CropImageView.kt
+++ milkrop/src/main/java/com/milkcocoa/info/milkrop/view/CropImageView.kt
@@ -1,6 +1,8 @@
 package com.milkcocoa.info.milkrop.view
 
 import android.content.Context
+import android.graphics.PointF
+import android.graphics.RectF
 import android.net.Uri
 import android.util.AttributeSet
 import android.view.ViewGroup
@@ -29,9 +31,58 @@ class CropImageView: FrameLayout {
     }
 
     init {
+        val maskView = CropMaskView(context).apply {
+            setAspectRatio(CropMaskView.AspectRatio(4f, 3f))
+        }
         gestureImageView = GestureImageView(context).apply {
+            setListener(object: GestureListener{
+                override fun onRotate(view: GestureImageView, rotation: Float) {
+                }
 
+                override fun onScale(view: GestureImageView, scaleFactor: Float) {
+                }
+
+                override fun onTranslate(view: GestureImageView, pivot: PointF, edge: RectF) {
+                }
+
+                override fun onRelease(view: GestureImageView) {
+                    val pivot = view.currentBitmapCenter()
+                    val factor = view.currentScale()
+                    val size = view.getBitmapSize()
+                    val edge = view.currentBitmapCorners()
+
+                    maskView.cropWindow()?.let { cropWindow ->
+
+                        var dx = 0f
+                        var dy = 0f
+
+                        if(edge.bottom < cropWindow.bottom){
+                            dy += -(edge.bottom - cropWindow.bottom)
+                        }
+
+                        if(edge.top > cropWindow.top){
+                            dy += -(edge.top - cropWindow.top)
+                        }
+
+                        if(edge.left > cropWindow.left){
+                            dx += -(edge.left - cropWindow.left)
+                        }
+                        if(edge.right < cropWindow.right){
+                            dx += -(edge.right - cropWindow.right)
+                        }
+
+                        view.translate(
+                            dx = dx.div(1.0f),
+                            dy = dy.div(1.0f),
+                            animation = false
+                        )
+                    }
+
+                }
+            })
         }
         addView(gestureImageView, LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+        addView(maskView, LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+
     }
 }
---
+++ milkrop/src/main/java/com/milkcocoa/info/milkrop/view/CropMaskView.kt
@@ -0,0 +1,84 @@
+package com.milkcocoa.info.milkrop.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+
+/**
+ * CropMaskView
+ * @author keita
+ * @since 2023/11/24 22:45
+ */
+
+/**
+ *
+ */
+class CropMaskView : View {
+    data class AspectRatio(val x: Float, val y: Float)
+
+
+    private val backgroundPaint = Paint()
+    private val paint = Paint()
+
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr )
+
+
+    private var ratio: AspectRatio? = null
+    fun setAspectRatio(ratio: AspectRatio){
+        this.ratio = ratio
+        invalidate()
+    }
+
+    private var cropWindow: RectF? = null
+    fun cropWindow() = cropWindow
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        setLayerType(LAYER_TYPE_HARDWARE, null)
+        cropWindow = null
+
+        //全体を描画する
+        backgroundPaint.color = Color.parseColor("#7F000000")
+        canvas.drawRect(
+            0f,
+            0f,
+            canvas.width.toFloat(),
+            canvas.height.toFloat(),
+            backgroundPaint
+        )
+
+        ratio ?: return
+
+        //全体の背景から円をくり抜く
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        val pivot = PointF(width.toFloat().div(2), height.toFloat().div(2))
+        cropWindow = calculateRect(ratio!!, pivot, 20f)
+
+
+        canvas.drawRect(
+            cropWindow!!,
+            paint
+        )
+    }
+
+    private fun calculateRect(ratio: AspectRatio, pivot: PointF, margin: Float): RectF {
+        val marginDp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, margin, context.applicationContext.resources.displayMetrics)
+        val fullWidth = width.minus(marginDp.times(2))
+        return RectF(
+            pivot.x - fullWidth.div(2.0f),
+            pivot.y - fullWidth.div(2.0f).times(ratio.y.div(ratio.x)),
+            pivot.x + fullWidth.div(2.0f),
+            pivot.y + fullWidth.div(2.0f).times(ratio.y.div(ratio.x)),
+        )
+    }
+}
