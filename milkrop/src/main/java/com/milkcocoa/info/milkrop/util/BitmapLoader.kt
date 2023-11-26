3bcc672116604ac5b0a9b270df636b87826aeb04
keita <developer@milkcocoa.info>
2023-11-26 21:51:43 +0900

    BitmapLoaderでダウンサンプルするように

--- milkrop/src/main/java/com/milkcocoa/info/milkrop/util/BitmapLoader.kt
+++ milkrop/src/main/java/com/milkcocoa/info/milkrop/util/BitmapLoader.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.util.Size
 import com.milkcocoa.info.milkrop.gesture.RotationGestureListener
 import com.milkcocoa.info.milkrop.view.GestureImageView
 import kotlinx.coroutines.CoroutineScope
@@ -27,15 +28,15 @@ import java.io.IOException
 object BitmapLoader {
     interface BitmapLoaderListener{
         fun onError(e: Throwable)
-        suspend fun onLoad(bitmap: Bitmap)
+        suspend fun onLoad(bitmap: Bitmap, sampleCount: Int)
     }
 
 
     class Option{
         var downSample: Boolean = false
 
-        var minSize: Int = 0
-        var maxSize: Int = Int.MAX_VALUE
+        var minSize: Size = Size(0, 0)
+        var maxSize: Size = Size(0, 0)
 
         var maxScale: Float = Float.POSITIVE_INFINITY
         var minScale: Float = 0f
@@ -52,7 +53,6 @@ object BitmapLoader {
     fun load(context: Context, uri: Uri, options: Option, listener: BitmapLoaderListener){
         val coroutineContext = Dispatchers.IO + Job()
         CoroutineScope(coroutineContext).launch {
-            delay(5000)
             kotlin.runCatching {
                 BitmapFactory.Options().apply {
                     inJustDecodeBounds = false
@@ -65,6 +65,19 @@ object BitmapLoader {
                     inJustDecodeBounds = false
                     inSampleSize = 1
 
+                    if(options.downSample){
+                        if(outHeight > options.maxSize.height || outWidth > options.maxSize.width){
+                            val halfHeight: Int = outHeight / 2
+                            val halfWidth: Int = outWidth / 2
+
+                            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+                            // height and width larger than the requested height and width.
+                            while (outHeight / inSampleSize >= options.maxSize.height && outWidth / inSampleSize >= options.maxSize.width) {
+                                inSampleSize *= 2
+                            }
+                        }
+                    }
+
 
                     BitmapFactory.decodeStream(
                         context.contentResolver.openInputStream(uri)?.buffered(8192),
@@ -72,7 +85,7 @@ object BitmapLoader {
                         this
                     )?.apply {
                         withContext(Dispatchers.Main){
-                            listener.onLoad(this@apply)
+                            listener.onLoad(this@apply, inSampleSize)
                         }
                     } ?: kotlin.run {
                         listener.onError(IOException())
@@ -85,8 +98,8 @@ object BitmapLoader {
     private val defaultOption = Option().apply {
         downSample = false
 
-        minSize = 0
-        maxSize = Int.MAX_VALUE
+        minSize = Size(0, 0)
+        maxSize = Size(1920, 1080)
 
         minScale = 0f
         maxScale = Float.POSITIVE_INFINITY
